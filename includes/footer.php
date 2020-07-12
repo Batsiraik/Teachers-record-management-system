dfcd1c7dfd513fd5e0ac6f9c512a479912a4b1da
batsie <46093695+Batsiraik@users.noreply.github.com>
2020-07-12 21:40:45 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer-area section_gap">
+        <div class="container" align="center">
+
+    <h2 align="center" style="color:#fff"> Teacher Records Management System</h2>
+    <span style="color:#fff" >&copy;<a href="" target="_blank"> BatsieBrilliant</a></span>
+        </div>
+    </footer>
